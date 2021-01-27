d4c83665980dd0f90393844c2f48a37a73809888
Eugeniov <bastien.coquelin@gmail.com>
2021-01-27 10:17:55 +0100

    add all prefab

---
+++ Assets/Infinitemove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infinitemove : MonoBehaviour
+{
+    private float lenght, startPosition;
+    public float parallaxe;
+    public GameObject MainCam;
+
+    public bool Infinite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = MainCam.transform.position.x * (1 - parallaxe);
+        float dist = MainCam.transform.position.x * parallaxe;
+
+        transform.position = new Vector3(startPosition + dist, transform.position.y, transform.position.z);
+
+        if (Infinite)
+        {
+            if (temp > startPosition + lenght)
+            {
+                startPosition += lenght;
+            }
+                
+            else if ( temp < startPosition - lenght)
+            {
+                startPosition -= lenght;
+            }
+                
+        }
+    }
+}
---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed;
+    public Camera playercamera;
+    [SerializeField]
+    private bool IsGrounded = false;
+
+    [SerializeField]
+    private BoxCollider2D boxCollider2d;
+    
+    [SerializeField]
+    private Rigidbody2D rigidbody2d;
+
+    [SerializeField]
+    private Animator animator;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        animator.SetBool("Start", true);
+
+        if(IsGrounded == true && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            float jumpVelocity = 10.0f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+            Debug.Log("Jump");
+            IsGrounded = false;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "plateform")
+        {
+            IsGrounded = true;
+            Debug.Log("IsGrounded");
+        }
+    }
+}
